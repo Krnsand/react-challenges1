177feb18c41b9ee7071721e19e2745120f9f1f70
Krnsand <krnsandin@gmail.com>
2023-04-23 11:47:08 +0000

    Add content to site using keys and lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from "./App.module.css";
 /* import NavBarSimple from "./components/NavBarSimple"; */
 import NavBarForm from "./components/NavBarForm";
 import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     {/*  <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
