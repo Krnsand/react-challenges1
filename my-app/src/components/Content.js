750662384251b8b315741f0c172a31b68b8fc6c6
Krnsand <krnsandin@gmail.com>
2023-04-29 08:32:52 +0000

    Create Loader

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,22 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import { savedPosts } from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
 
     render() {
@@ -18,23 +30,22 @@ class Content extends Component {
 
                 <div className={css.SearchResults}>
 
-                    {/* Part 1: Creating the map function */}
-
                     {
-                        savedPosts.map((post) => {
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random" />
-                                <p>{post.description}</p>
-                            </div>
-                        })
+                        /*        savedPosts.map((post) => {
+                                    return <div className={css.SearchItem} key={post.title}>
+                                        <p>{post.title}</p>
+                                        <p>{post.name}</p>
+                                        <img src={post.image} alt="random" />
+                                        <p>{post.description}</p>
+                                    </div>
+                               })
+                            */
+
+                        this.state.isLoaded ?
+                            <PostItem savedPosts={savedPosts} />
+                            : <Loader />
                     }
 
-
-                    {/* Part 2: Creating a child component */}
-
-                    <PostItem savedPosts={savedPosts} />
                 </div>
             </div>
         )
